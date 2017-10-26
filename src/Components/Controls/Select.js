fd975867e63d9adea60e39eb2c99bc1170dd961e
Hachem <HackSek@users.noreply.github.com>
2017-10-26 16:43:41 +0100

    First commit

---
+++ src/Components/Controls/Select.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+
+class Select extends Component {
+
+    constructor(props) {
+
+      super(props);
+      this.state = {
+
+        value: props.value
+
+      }
+
+      this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(e) {
+
+      this.setState({value: e.target.value}, function() {
+
+         this.props.onChange(this.state.value);
+        
+      });
+
+    }
+
+  render() {
+
+    return(
+
+       <div className="select" onChange={this.onChange}>
+
+           <select>
+
+              <option value="text">Text</option>
+
+              <option value="html">HTML</option>
+
+           </select>
+
+       </div>
+
+    );
+
+  }
+
+}
+
+export default Select;
+
---
+++ src/Components/Controls/Text.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+
+class Text extends Component {
+
+    constructor(props) {
+        
+              super(props);
+              this.state = {
+        
+                value: props.value
+        
+              }
+        
+              this.onChange = this.onChange.bind(this);
+            }
+        
+            onChange(e) {
+        
+              this.setState({value: e.target.value}, function() {
+        
+                 this.props.onChange(this.state.value);
+                
+              });
+        
+            }
+        
+          render() {
+        
+            return(
+        
+               <div className="select" onChange={this.onChange}>
+        
+                   <input className="form-control" type="number" value={this.state.value} onChange={this.onChange} />
+        
+               </div>
+        
+            );
+        
+          }
+
+}
+
+export default Text;
